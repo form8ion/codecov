f73df62c0e4477d2c110a2c746f450b64b685264
Matt Travi <programmer@travi.org>
2023-08-06 11:41:57 -0500

    build(rollup): transpilation is no longer done to bring the support level below `engines.node`
    BREAKING CHANGE: the bundle was previously tranpiled to support node versions down to v12.20. that
    transpilation no longer happens, so no support is promised beyond what is defined in `engines.node`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,10 @@
 import autoExternal from 'rollup-plugin-auto-external';
-import babel from '@rollup/plugin-babel';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
   plugins: [
     autoExternal(),
-    babel({
-      babelrc: false,
-      exclude: ['./node_modules/**'],
-      presets: [['@form8ion', {targets: {node: '12.20'}, modules: false}]]
-    }),
     nodeResolve({mainFields: ['module']})
   ],
   output: [
