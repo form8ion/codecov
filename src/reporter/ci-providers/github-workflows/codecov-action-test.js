b31b89f2ea86ffa31256db4382496e468fce3752
Matt Travi <programmer@travi.org>
2024-02-08 23:36:43 -0600

    test(action): grouped sub-suites

--- src/reporter/ci-providers/github-workflows/codecov-action-test.js
+++ src/reporter/ci-providers/github-workflows/codecov-action-test.js
@@ -4,24 +4,28 @@ import any from '@travi/any';
 import {findCodecovActionIn, scaffold} from './codecov-action.js';
 
 suite('codecov action', () => {
-  test('that the codecov action is scaffolded', () => {
-    assert.deepEqual(scaffold(), {uses: 'codecov/codecov-action@v3'});
+  suite('scaffold', () => {
+    test('that the codecov action is scaffolded', () => {
+      assert.deepEqual(scaffold(), {uses: 'codecov/codecov-action@v3'});
+    });
   });
 
-  test('that the codecov action is returned from the list of steps', async () => {
-    const codecovAction = {...any.simpleObject(), uses: 'codecov/codecov-action@v1'};
+  suite('find in steps', () => {
+    test('that the codecov action is returned from the list of steps', async () => {
+      const codecovAction = {...any.simpleObject(), uses: 'codecov/codecov-action@v1'};
 
-    assert.equal(
-      findCodecovActionIn([
-        ...any.listOf(any.simpleObject),
-        codecovAction,
-        ...any.listOf(any.simpleObject)
-      ]),
-      codecovAction
-    );
-  });
+      assert.equal(
+        findCodecovActionIn([
+          ...any.listOf(any.simpleObject),
+          codecovAction,
+          ...any.listOf(any.simpleObject)
+        ]),
+        codecovAction
+      );
+    });
 
-  test('that `undefined` is returned when the codecov action is not found in the list of steps', async () => {
-    assert.isUndefined(findCodecovActionIn(any.listOf(any.simpleObject)));
+    test('that `undefined` is returned when the codecov action is not found in the list of steps', async () => {
+      assert.isUndefined(findCodecovActionIn(any.listOf(any.simpleObject)));
+    });
   });
 });
