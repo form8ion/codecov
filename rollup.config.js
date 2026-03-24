a3922c2cf42b9af02653b3b9f158f2435fe0d6c9
Matt Travi <programmer@travi.org>
2026-03-23 21:13:09 -0500

    ci(codecov): upload bundle analysis

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import autoExternal from 'rollup-plugin-auto-external';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {codecovRollupPlugin} from '@codecov/rollup-plugin';
 
 export default {
   input: 'src/index.js',
   plugins: [
     autoExternal(),
-    nodeResolve({mainFields: ['module']})
+    nodeResolve({mainFields: ['module']}),
+    codecovRollupPlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: '@form8ion/rollup',
+      uploadToken: process.env.CODECOV_TOKEN
+    })
   ],
   output: [
     {file: 'lib/index.js', format: 'esm', sourcemap: true}
