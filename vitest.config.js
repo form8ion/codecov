46bb0dcb99969912a38ec68dcdefb343b8eb0e4d
Matt Travi <programmer@travi.org>
2026-03-23 21:06:08 -0500

    test(coverage): namespace the unit test coverage to make room for integration

--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text-summary', 'html'],
+      reportsDirectory: 'coverage/unit',
       include: ['src/**'],
       exclude: ['src/**/index.js']
     }
